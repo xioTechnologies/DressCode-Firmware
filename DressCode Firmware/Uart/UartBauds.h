f6ffe8ad3661f43f57a424508baf2cd957ebc72b
xioTechnologies <social@x-io.co.uk>
2013-05-12 00:12:35 +0100

    removed unnecessary typedef from enum

--- DressCode Firmware/Uart/UartBauds.h
+++ DressCode Firmware/Uart/UartBauds.h
@@ -1,6 +1,8 @@
 /*
     UartBauds.h
     Author: Seb Madgwick
+
+    UxBRG register values calculated for FCY = 4 MIPS and BRGH = 1.
 */
 
 #ifndef Uartbauds_h
@@ -9,11 +11,11 @@
 //------------------------------------------------------------------------------
 // Definitions
 
-typedef enum {
+enum UartBaud {
     UART_BAUD_9600      = 103,
     UART_BAUD_115200    = 8,
     UART_BAUD_250000    = 3
-} UartBaud;
+};
 
 #endif
 
